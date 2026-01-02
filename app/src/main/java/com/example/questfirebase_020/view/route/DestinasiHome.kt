a34568d592c860db9800db498777b89f3c65823a
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-01-02 20:38:03 +0700

    Menambahkan objek DestinasiHome untuk definisi route navigasi halaman utama

---
+++ app/src/main/java/com/example/questfirebase_020/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.questfirebase_020.view.route
+
+import com.example.questfirebase_020.R
+import com.example.questfirebase_020.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
