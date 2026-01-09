e1a608c305c66b96c560806e99130d539c7ddf9a
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-01-09 20:16:49 +0700

    menambah navigasi untuk edit screen

--- app/src/main/java/com/example/questfirebase_020/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/questfirebase_020/view/controlNavigasi/PetaNavigasi.kt
@@ -62,6 +62,11 @@ fun HostNavigasi(
                 navigateBack = {navController.navigate(DestinasiHome.route)})
         }
 
-
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg){
+            type = NavType.StringType })
+        ){
+            EditSiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)},
+                onNavigateUp = {navController.navigateUp()})
+        }
     }
 }
