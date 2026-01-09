20aace262600c40ad7ef8235703b96cf1dd6fdb2
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-01-09 20:21:36 +0700

    nambah fungsi delete untuk data siswa

--- app/src/main/java/com/example/questfirebase_020/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_020/viewmodel/DetailViewModel.kt
@@ -48,5 +48,12 @@ class DetailViewModel(savedStateHandle: SavedStateHandle, private val repository
         }
     }
 
-
+    suspend fun hapusSatuSiswa() {
+        try {
+            repositorySiswa.hapusSatuSiswa(idSiswa)
+            println("Sukses Hapus Data: $idSiswa")
+        } catch (e: Exception) {
+            println("Gagal Hapus Data: ${e.message}")
+        }
+    }
 }
