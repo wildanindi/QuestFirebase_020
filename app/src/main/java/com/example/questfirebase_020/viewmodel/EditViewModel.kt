4583b3091acc7d02a0f0b9ffad61249acdff76a4
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-01-09 20:24:36 +0700

    tambah fungsi update state form dan validasi input

--- app/src/main/java/com/example/questfirebase_020/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_020/viewmodel/EditViewModel.kt
@@ -34,5 +34,16 @@ class EditViewModel(
         }
     }
 
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
 
 }
