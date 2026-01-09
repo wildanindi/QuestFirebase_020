24cfa8b82d11db5ecf78a45c9e27888894aa0a23
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-01-09 20:23:43 +0700

    setup struktur EditViewModel dan ambil ID siswa dari navigasi

--- app/src/main/java/com/example/questfirebase_020/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebase_020/viewmodel/EditViewModel.kt
@@ -1,2 +1,31 @@
 package com.example.questfirebase_020.viewmodel
 
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_020.modeldata.DetailSiswa
+import com.example.questfirebase_020.modeldata.UIStateSiswa
+import com.example.questfirebase_020.modeldata.toDataSiswa
+import com.example.questfirebase_020.modeldata.toUiStateSiswa
+import com.example.questfirebase_020.repositori.RepositorySiswa
+import com.example.questfirebase_020.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+
+}
