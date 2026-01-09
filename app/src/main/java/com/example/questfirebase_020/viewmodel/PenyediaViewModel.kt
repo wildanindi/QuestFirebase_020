f73ae0078f794e44a917f47dbc682b406b5dac03
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-01-09 20:27:01 +0700

    nambah initializer untuk EditViewModel

--- app/src/main/java/com/example/questfirebase_020/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase_020/viewmodel/PenyediaViewModel.kt
@@ -20,6 +20,11 @@ object PenyediaViewModel {
                 aplikasiDataSiswa().container.repositorySiswa
             )
         }
-
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
