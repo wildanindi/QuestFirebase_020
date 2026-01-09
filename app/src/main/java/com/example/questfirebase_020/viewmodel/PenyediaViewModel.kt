a606430f188f1e9eef3e631043123cab32c8ee6a
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-01-09 20:26:45 +0700

    nambah initializer untuk DetailViewModel

--- app/src/main/java/com/example/questfirebase_020/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase_020/viewmodel/PenyediaViewModel.kt
@@ -1,23 +1,25 @@
 package com.example.questfirebase_020.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questfirebase_020.repositori.AplikasiDataSiswa
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-                AplikasiDataSiswa
-        )
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
         }
-        initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
-        }
+
     }
 }
