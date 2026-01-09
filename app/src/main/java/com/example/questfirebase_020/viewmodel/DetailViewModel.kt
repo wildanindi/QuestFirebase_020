693f03e31c3128badeefdc3edf76431c1a6f1617
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-01-09 20:19:58 +0700

    setup DetailViewModel structure and StatusUIDetail interface

--- app/src/main/java/com/example/questfirebase_020/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_020/viewmodel/DetailViewModel.kt
@@ -1,2 +1,35 @@
+
+@file:OptIn(InternalSerializationApi::class)
+
 package com.example.questfirebase_020.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_020.modeldata.Siswa
+import com.example.questfirebase_020.repositori.RepositorySiswa
+import com.example.questfirebase_020.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa) : ViewModel() {
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+
+}
