b72cdeadbf03c8ef567cf091b7b62bab17010bdc
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-01-09 20:29:30 +0700

    nambah suspend fun untuk editSatuSiswa

--- app/src/main/java/com/example/questfirebase_020/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/questfirebase_020/repositori/RepositorySiswa.kt
@@ -59,7 +59,18 @@ class FirebaseRepositorySiswa : RepositorySiswa {
         }
     }
 
-
+    override suspend fun editSatuSiswa(id: Long, siswa: Siswa) {
+        val docQuery = collection.whereEqualTo("id", id).get().await()
+        val docId = docQuery.documents.firstOrNull()?.id ?: return
+        collection.document(docId).set(
+            mapOf(
+                "id" to siswa.id,
+                "nama" to siswa.nama,
+                "alamat" to siswa.alamat,
+                "telpon" to siswa.telpon
+            )
+        ).await()
+    }
 
     override suspend fun hapusSatuSiswa(id: Long) {
         val docQuery = collection.whereEqualTo("id", id).get().await()
